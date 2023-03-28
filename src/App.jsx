0392d9661620a56e68f28f3eec1f5072b7a23f85
unknown <bbabines@gmail.com>
2023-03-27 22:19:35 -0400

    Imported React Flow into App and added boilerplate code.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import "./App.css";
+import React from "react";
+import ReactFlow from "reactflow";
 
-function App() {
+import "reactflow/dist/style.css";
+
+const initialNodes = [
+	{ id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
+	{ id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
+];
+const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
+
+export default function App() {
 	return (
-		<div className="App">
-			<h1>Hi</h1>
+		<div style={{ width: "100vw", height: "100vh" }}>
+			<ReactFlow nodes={initialNodes} edges={initialEdges} />
 		</div>
 	);
 }
-
-export default App;
