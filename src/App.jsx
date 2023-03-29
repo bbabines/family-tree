c90996a6fbebd723fadfce5b0480238e0f2f8d22
unknown <bbabines@gmail.com>
2023-03-28 21:15:18 -0400

    feet: Added additional interactivity to the boilerplate.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import ReactFlow from "reactflow";
+import React, { useCallback } from "react";
+import ReactFlow, { useNodesState, useEdgesState, addEdge } from "reactflow";
 
 import "reactflow/dist/style.css";
 
@@ -10,9 +10,23 @@ const initialNodes = [
 const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
 
 export default function App() {
+	const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+	const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+	const onConnect = useCallback(
+		(params) => setEdges((eds) => addEdge(params, eds)),
+		[setEdges]
+	);
+
 	return (
 		<div style={{ width: "100vw", height: "100vh" }}>
-			<ReactFlow nodes={initialNodes} edges={initialEdges} />
+			<ReactFlow
+				nodes={nodes}
+				edges={edges}
+				onNodesChange={onNodesChange}
+				onEdgesChange={onEdgesChange}
+				onConnect={onConnect}
+			/>
 		</div>
 	);
 }
